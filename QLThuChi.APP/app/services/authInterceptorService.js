cc8b2fb4237b896bffbc900fcf4e8048740c252d
Phạm Văn Vinh <vinhpv@outlook.com>
2016-10-24 14:12:34 +0700

    Commmit

---
+++ QLThuChi.APP/app/services/authInterceptorService.js
@@ -0,0 +1,39 @@
+﻿'user strict'
+app.factory('authInterceptorService', ['$q', '$injector', '$location', '$localStorage', function ($q, $injector, $location, $localStorage) {
+
+    var authInterceptorServiceFactory = {};
+
+    var _request = function (config) {
+
+        config.headers = config.headers || {};
+
+        var authData = $localStorage.authorizationData;
+        if (authData) {
+            config.headers.Authorization = 'Bearer ' + authData.token;
+        }
+
+        return config;
+    }
+
+    var _responseError = function (rejection) {
+        if (rejection.status === 401) {
+            var authService = $injector.get('authService');
+            var authData = $localStorage.authorizationData;
+
+            //if (authData) {
+            //    if (authData.useRefreshTokens) {
+            //        $location.path('/refresh');
+            //        return $q.reject(rejection);
+            //    }
+            //}
+            authService.logOut();
+            $location.path('/login');
+        }
+        return $q.reject(rejection);
+    }
+
+    authInterceptorServiceFactory.request = _request;
+    authInterceptorServiceFactory.responseError = _responseError;
+
+    return authInterceptorServiceFactory;
+}]);
---
+++ QLThuChi.APP/app/services/authService.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+app.factory('authService', ['$http', '$q', '$localStorage', 'ngAuthSettings',
+    function ($http, $q, localStorageService, ngAuthSettings) {
+        var sefl = this;
+        var _authentication = {
+            isAuth: false,
+            userName: ""
+        };
+        var serviceBase = ngAuthSettings.serviceBase;
+        return {
+            sefl.loGin = function(loginData)
+            {
+                var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+                var deferred = $q.defer();
+
+                $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+
+                    $localStorage.authorizationData =
+                    { token: response.access_token, userName: loginData.userName };
+
+                    _authentication.isAuth = true;
+                    _authentication.userName = loginData.userName;
+                    
+                    deferred.resolve(response);
+
+                }).error(function (err, status) {
+                    logOut();
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            },
+            sefl.logOut = function()
+            {
+                $localStorage.authorizationData = null;
+                _authentication.isAuth = false;
+                _authentication.userName = "";
+            },
+            sefl.authentication = _authentication
+        }
+    }
+]
+)
